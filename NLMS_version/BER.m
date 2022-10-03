411ca439323ded1a57b23b0f1014597861d0c407
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-10-04 00:16:05 +0800

    Update BER.m

--- NLMS_version/BER.m
+++ NLMS_version/BER.m
@@ -3,6 +3,8 @@
 % 
 function error_rate = BER(true_data, ans_data)
     error_count = sum(ans_data ~= true_data);
+    %
     % error rate = amount of error / amount of data
     error_rate = error_count / length(true_data); 
 return
+
