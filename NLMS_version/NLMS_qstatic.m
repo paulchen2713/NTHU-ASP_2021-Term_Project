01fca8f3e04a1d2da102f3b8abdbb2d4aadea6f3
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-10-08 14:45:01 +0800

    Update NLMS_qstatic.m

--- NLMS_version/NLMS_qstatic.m
+++ NLMS_version/NLMS_qstatic.m
@@ -33,16 +33,16 @@ function ans_qstatic = NLMS_qstatic(train, data, L_order, step_size)
             output_z(i) = sign(output_y(i));
         else
             if count <= 200 && mod(i, 1200) <= 200 && mod(i,1200) > L_order-1
-                desired_d(i)=train(count);
-                output_z(i)=sign(output_y(i));
-                count=count+1;
+                desired_d(i) = train(count);
+                output_z(i) = sign(output_y(i));
+                count = count + 1;
             else
-                count=L_order;
-                desired_d(i)=sign(output_y(i));
-                output_z(i)=sign(output_y(i));
+                count = L_order;
+                desired_d(i) = sign(output_y(i));
+                output_z(i) = sign(output_y(i));
             end
         end
-        error(i) = desired_d(i)-output_y(i);
+        error(i) = desired_d(i) - output_y(i);
         weight = weight + step_size * error(i) * ref / (eps + ref.' * ref);
     end
     %
@@ -50,7 +50,8 @@ function ans_qstatic = NLMS_qstatic(train, data, L_order, step_size)
     %
     result = [];
     for h = 0 : 199
-        result = [result output_z(len_train + 1 + h*1200 : 1200+h*1200)];
+        result = [result output_z(len_train + 1 + h*1200 : 1200 + h*1200)];
     end
     ans_qstatic = result;
 return
+
